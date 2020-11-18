52bcf6d25ec37fbc0ed1f33c4605d4828f538980
abhisheksaini <saini.abhishek@hotmail.com>
2020-11-18 23:40:26 +0100

    create dynamic element --- cookie message.

--- script.js
+++ script.js
@@ -7,25 +7,39 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const header = document.querySelector('.header');
 
-const openModal = function () {
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+const openModal = function (event) {
+    event.preventDefault();
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
 };
 
 const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach((btn) => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
 document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-    closeModal();
-  }
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
+    }
+});
+
+// cookie message element
+const message = document.createElement('div');
+message.classList.add('cookie-message')
+message.innerHTML = 'We use cookies to improve functionality and analytics. ' +
+    '<button class="btn btn--close-cookie">Got it!</button>';
+header.before(message);
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+message.style.height = Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+document.querySelector('.btn--close-cookie').addEventListener('click', function () {
+   message.remove();
 });
