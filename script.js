05e271827e1a2e57260c927c6a106c0995282b92
abhisheksaini <saini.abhishek@hotmail.com>
2020-11-21 21:22:19 +0100

    menu fade - bind

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ const navLinks = document.querySelector('.nav__links');
 const tabs = document.querySelectorAll('.operations__tab');
 const tabsContainer = document.querySelector('.operations__tab-container');
 const tabsContent= document.querySelectorAll('.operations__content');
+const nav = document.querySelector('.nav');
 
 const openModal = function (event) {
     event.preventDefault();
@@ -86,3 +87,21 @@ tabsContainer.addEventListener('click', function(e) {
     tabsContent.forEach((content) => content.classList.remove('operations__content--active'));
     document.querySelector(`.operations__content--${btn.dataset.tab}`).classList.add('operations__content--active');
 });
+
+// menu fade animation
+const navHover = function (e) {
+    if (e.target['classList'].contains('nav__link')) {
+        const link = e.target;
+        const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+        const logo = link.closest('.nav').querySelector('img');
+
+        siblings.forEach((el) => {
+            if (el !== link) el.style.opacity = this;
+        });
+        logo.style.opacity = this;
+    }
+};
+
+// bind will return a new function, passing "argument" into handler
+nav.addEventListener('mouseover', navHover.bind(0.5));
+nav.addEventListener('mouseout', navHover.bind(1));
